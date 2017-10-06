34affe04fa81f7732c326625e091abed4e3359b4
Fritz <fre.healy@gmail.com>
2017-10-06 13:19:08 -0400

    minor styling homepage

--- template-parts/content-boxes.php
+++ template-parts/content-boxes.php
@@ -5,25 +5,31 @@ if($boxes):?>
             $box = $boxes[$i-1];?>
             <div class="row-<?php echo $i;?> box">
                 <?php if($box['title']):?>
-                    <header><h2><?php echo $box['title'];?></h2></header>
+                    <header class="js-blocks"><h2><?php echo $box['title'];?></h2></header>
                 <?php endif;?>
-                <?php if($box['links']):?>
-                    <div class="links">
-                        <?php foreach($box['links'] as $link):?>
-                            <div class="link">
-                                <a href="<?php echo $link['link'];?>">
-                                    <?php echo $link['link_text'];?>
+                <?php if(($box['more_link']&&$box['more_text'])||$box['title']):?>
+                    <div class="wrapper">
+                        <?php if($box['links']):?>
+                            <div class="links">
+                                <?php foreach($box['links'] as $link):?>
+                                    <div class="link">
+                                        <a href="<?php echo $link['link'];?>">
+                                            <?php echo $link['link_text'];?>
+                                        </a>
+                                    </div><!--.link-->
+                                <?php endforeach;?>
+                            </div><!--.links-->
+                        <?php endif;?>
+                        <?php if($box['more_link']&&$box['more_text']):?>
+                            <div class="more">
+                                <a href="<?php echo $box['more_link'];?>">
+                                    <div class="wrapper">
+                                        <?php echo $box['more_text'];?>
+                                    </div><!--.wrapper-->
                                 </a>
-                            </div><!--.link-->
-                        <?php endforeach;?>
-                    </div><!--.links-->
-                <?php endif;?>
-                <?php if($box['more_link']&&$box['more_text']):?>
-                    <div class="more">
-                        <a href="<?php echo $box['more_link'];?>">
-                            <?php echo $box['more_text'];?>
-                        </a>
-                    </div><!--.more-->
+                            </div><!--.more-->
+                        <?php endif;?>
+                    </div><!--.wrapper-->
                 <?php endif;?>
             </div><!--.box-->
         <?php endfor;?>
